1d157b4bde5c6bc72021f56760d2e6a7778b09ef
Dixit Bhuva <mail@dixit.dev>
2025-01-08 10:22:51 +0530

    done

--- README.md
+++ README.md
@@ -19,10 +19,13 @@ pre patched dwm
 
 Arch Linux
 
+run this from home directory and dont deleted cloned folder after install because it contain configrations and wallpapers
+copy config and make it yours then delete this folder
+
 ```bash
-git clone https://github.com/dixitbhuva/dwm.git \
-&& cd dwm \
-&& sudo pacman -S base-devel libx11 libxft libxinerama terminus-font freetype2 fontconfig kitty dmenu \
+git clone https://github.com/dixitbhuva/dwm.git ~/.dwm \
+&& cd ~/.dwm \
+&& sudo pacman -S xorg-xsetroot kitty dmenux org-xset feh dunst polkit-gnome playerctl base-devel brightnessctl libx11 libxft libxinerama terminus-font freetype2 fontconfig \
 && make \
 && sudo make clean install
 ```
@@ -33,4 +36,23 @@ Dont Forget to create desktop file or you can just put this to your .xinitrc if 
 while true; do
 	dwm 2> ~/.dwm.log
 done
+```
+
+Assuming you have pipewire otherwise change to one of this in config file if you have pulse audio
+
+```c
+/* If you use pulsaudio add somewhere in your constants definition section instead. */
+static const char *upvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "+5%", NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "0", "-5%", NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "0", "toggle", NULL };
+
+/* AidenThing suggests using this general solution for dynamically changing outputs. */
+static const char *upvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "+5%", NULL };
+static const char *downvol[] = { "/usr/bin/pactl", "set-sink-volume", "@DEFAULT_SINK@", "-5%", NULL };
+static const char *mutevol[] = { "/usr/bin/pactl", "set-sink-mute", "@DEFAULT_SINK@", "toggle", NULL };
+
+/* If you use amixer, use this instead. Thanks go to DaniOrt3ga. */
+static const char *upvol[] = { "/usr/bin/amixer", "set", "Master", "5%+", NULL };
+static const char *downvol[] = { "/usr/bin/amixer", "set", "Master", "5%-", NULL };
+static const char *mutevol[] = { "/usr/bin/amixerl", "set", "Master", "toggle", NULL };
 ```
--- config.def.h
+++ config.def.h
@@ -1,5 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
+#include <X11/XF86keysym.h>
+
 /* appearance */
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
@@ -10,21 +12,26 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;        /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "Terminus:style=Bold:pixelsize=12:antialias=true:autohint=true", "monospace:size=10" };
-static const char dmenufont[]       = "Terminus:style=Bold:pixelsize=12:antialias=true:autohint=true";
+static const char *fonts[]          = { "Terminus:pixelsize=14:antialias=true:autohint=true", "monospace:size=10" };
+static const char dmenufont[]       = "Terminus:pixelsize=16:antialias=true:autohint=true";
 static const char col_gray1[]       = "#222222";
 static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#ffffff";
+static const char col_gray3[]       = "#ffffff";
 static const char col_cyan[]        = "#0090c9";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
-	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+	[SchemeSel]  = { col_gray3, col_cyan,  col_cyan  },
 };
 
 static const char *const autostart[] = {
-	"kitty", NULL,
+  "/usr/bin/xset", "s", "off", NULL, /* Disable screen saver (no blanking, no dimming) */ 
+  "/usr/bin/xset", "-dpms", NULL, /* Disable Display Power Management (no standby, suspend, or power-off) */
+  "/usr/bin/dbus-update-activation-environment", "--systemd", "--all", NULL, /* Don't Exactly know which variables are updated but it's good idea to run it with startup, seen on forms */
+  "/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1", NULL, /* Just Polkit, prompt for authentication in like gnome-disks */
+  "/usr/bin/dunst", NULL, /* Notification Daemon */
+  "sh", "-c", "/usr/bin/feh --no-fehbg --bg-fill ~/.dwm/wallpaper.png", NULL,
+  "sh", "-c", "while :; do ~/.dwm/dwmstatus.sh -; sleep 1; done", NULL,
 	NULL /* terminate */
 };
 
@@ -70,23 +77,45 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-l", "20", NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray3, "-l", "20", NULL };
 static const char *termcmd[]  = { "kitty", NULL };
 
+static const char *upvol[]      = { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "5%+",   "-l", "1",   NULL };
+static const char *downvol[]    = { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "5%-",   "-l", "1",   NULL };
+static const char *mutevol[]    = { "/usr/bin/wpctl",   "set-mute",   "@DEFAULT_AUDIO_SINK@",      "toggle",   NULL };
+
+static const char *playpausecmd[] = { "playerctl", "play-pause", NULL };
+static const char *nextcmd[] = { "playerctl", "next", NULL };
+static const char *prevcmd[] = { "playerctl", "previous", NULL };
+
+static const char *light_up[]   = { "/usr/bin/brightnessctl",   "set", "+10%", NULL };
+static const char *light_down[] = { "/usr/bin/brightnessctl",   "set", "10%-", NULL };
+
 static const Key keys[] = {
-	/* modifier                     key        function        argument */
-	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
-	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
-	{ MODKEY,                       XK_b,      togglebar,      {0} },
-	{ MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
-	{ MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
-	{ MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
-	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
-	{ MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
-	{ MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
-	{ MODKEY,                       XK_Return, zoom,           {0} },
-	{ MODKEY,                       XK_Tab,    view,           {0} },
-	{ MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
+  /* modifier                     key        function        argument */
+  { 0,                       XF86XK_AudioLowerVolume, spawn, {.v = downvol } },
+  { 0,                       XF86XK_AudioRaiseVolume, spawn, {.v = upvol   } },
+  { 0,                       XF86XK_AudioMute, spawn, {.v = mutevol } },
+
+  { 0,                         XF86XK_AudioPlay, spawn,                  {.v = playpausecmd } },
+  { 0,                         XF86XK_AudioNext, spawn,                  {.v = nextcmd } },
+  { 0,                         XF86XK_AudioPrev, spawn,                  {.v = prevcmd } },
+
+  { 0,				XF86XK_MonBrightnessUp,		spawn,	{.v = light_up} },
+	{ 0,				XF86XK_MonBrightnessDown,	spawn,	{.v = light_down} },
+
+  { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+  { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
+  { MODKEY,                       XK_b,      togglebar,      {0} },
+  { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
+  { MODKEY,                       XK_k,      focusstack,     {.i = -1 } },
+  { MODKEY,                       XK_i,      incnmaster,     {.i = +1 } },
+  { MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },
+  { MODKEY,                       XK_h,      setmfact,       {.f = -0.05} },
+  { MODKEY,                       XK_l,      setmfact,       {.f = +0.05} },
+  { MODKEY,                       XK_Return, zoom,           {0} },
+  { MODKEY,                       XK_Tab,    view,           {0} },
+  { MODKEY|ShiftMask,             XK_c,      killclient,     {0} },
 	{ MODKEY,                       XK_t,      setlayout,      {.v = &layouts[0]} },
 	{ MODKEY,                       XK_f,      setlayout,      {.v = &layouts[1]} },
 	{ MODKEY,                       XK_m,      setlayout,      {.v = &layouts[2]} },
