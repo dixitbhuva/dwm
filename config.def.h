0bd1fef94554319808d77b3f7ebe247e7fe3b8b1
Dixit Bhuva <mail@dixit.dev>
2025-02-21 12:15:47 +0530

    last used

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
 static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
+static const int topbar             = 0;        /* 0 means bottom bar */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=14";
 static const char col_gray1[]       = "#222222";
@@ -35,8 +35,10 @@ static const Rule rules[] = {
   /* class      instance    title       tags mask     isfloating   monitor */
   { "Gimp",     NULL,       NULL,       0,            1,           -1 },
   { "gimp",     NULL,       NULL,       0,            1,           -1 },
-  { "Firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
-  { "firefox",  NULL,       NULL,       1 << 1,       0,           -1 },
+  { "Firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
+  { "firefox",  NULL,       NULL,       1 << 2,       0,           -1 },
+  { "chromium", NULL,       NULL,       1 << 1,       0,           -1 },
+  { "Chromium", NULL,       NULL,       1 << 1,       0,           -1 },
 };
 
 /* layout(s) */
@@ -66,7 +68,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", col_gray1, "-nf", col_gray3, "-sb", col_cyan, "-sf", col_gray4, "-l", "20", NULL };
-static const char *termcmd[]  = { "alacritty", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 
 static const char *upvol[]      = { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "5%+",   "-l", "1",   NULL };
 static const char *downvol[]    = { "/usr/bin/wpctl",   "set-volume", "@DEFAULT_AUDIO_SINK@",      "5%-",   "-l", "1",   NULL };
@@ -92,7 +94,9 @@ static const Key keys[] = {
   { 0,				                    XF86XK_MonBrightnessUp,		spawn,	{.v = light_up} },
   { 0,				                    XF86XK_MonBrightnessDown,	spawn,	{.v = light_down} },
 
-  { MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
+  { MODKEY,                       XK_p,      spawn,          SHCMD("rofi -show drun") },
+
+  { MODKEY|ShiftMask,             XK_p,      spawn,          {.v = dmenucmd } },
   { MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
   { MODKEY,                       XK_b,      togglebar,      {0} },
   { MODKEY,                       XK_j,      focusstack,     {.i = +1 } },
